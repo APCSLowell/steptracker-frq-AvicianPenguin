277b3349c06c8789049e645a081ba4c25f90d736
Penguin <aleksandervolkov2007@gmail.com>
2025-04-21 16:50:08 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,33 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+ private int numSteps;
+ private int numRecordings;
+ private int numActive
+ public StepTracker(Steps)
+  {
+   numSteps = steps;
+   numRecordings = 0;
+   numActive = 0;
+  }
+ public void addDailySteps(x)
+  {
+   numSteps += x;
+   numRecordings += 1;
+   if(x>=10000)
+    numActive+=1;
+  }
+ public int activeDays()
+  {
+   return numActive;
+  }
+ public int averageSteps()
+  {
+   return ((double)numSteps/numRecordings);
+  }
+}
+                          
+ 
+            
+  
 } 
